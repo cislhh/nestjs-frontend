045bb166292c07a6b1d952247951148d7e216a1e
Azo <1353761785@qq.com>
2024-12-01 12:19:47 +0800

    配置路径别名

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "@/router/index.ts";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router); // 2.引用router
+app.mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import { App } from "vue";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+
+const routes = [
+  {
+    path: "/login",
+    component: () => import("@/views/login/login.vue"),
+    // component: () => import("../views/login/login.vue"),
+  },
+  {
+    path: "/reg",
+    component: () => import("@/views/login/reg.vue"),
+  },
+] as RouteRecordRaw[];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
+// export function setupRouter(app: App<Element>) {
+//   app.use(router);
+// }
